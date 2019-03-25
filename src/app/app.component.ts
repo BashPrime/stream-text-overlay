43c301a6c39efab9a476c5f2928884eb1587fd53
Evan Taylor <etaylor8086@gmail.com>
2019-03-24 18:55:00 -0700

    Set up basic text animations on an interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('visible', style({
+        opacity: 1
+      })),
+      state('hidden', style({
+        opacity: 0
+      })),
+      transition('visible => hidden', [
+        animate('0.5s')
+      ]),
+      transition('hidden => visible', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'stream-text-overlay';
+  visible = true;
+  count = 0;
+
+  ngOnInit() {
+    interval(5000).subscribe(x => {
+      this.toggle();
+    });
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
+
+  onDone(event) {
+    // We want to immediately show the text again upon hiding
+    if (event.toState === 'hidden') {
+      this.count++;
+      this.toggle();
+    }
+  }
 }
